4555e967cd08e7d2300c11770ce01c41e5dc2ae0
Daniel Vo <daniel.vo@berkeley.edu>
2021-09-30 10:02:03 -0700

    add event listener to all save buttons

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 setInterval( function(){
-	var now = moment().format('MMM Do YYYY hh:mm:ss' );
+	var now = moment().format('MMM Do YYYY hh:mm:ss');
 	$('#currentDay').text(now);
 }, 0)
+
+
+
+
+
+
+
+//when save button is pressed the input items are saved
+document.querySelectorAll('#button-addon2').forEach(item => {
+	item.addEventListener('click', event => {
+		console.log('saved');
+	})
+})
 
